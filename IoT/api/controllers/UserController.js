0783b15463451e575656084686994aa5a51b59e5
Sravan <sravan@Sravabns-MacBook-Pro.local>
2019-10-23 19:14:50 -0300

    Initial login added

--- IoT/api/controllers/UserController.js
+++ IoT/api/controllers/UserController.js
@@ -6,7 +6,34 @@
  */
 
 module.exports = {
-  
 
-};
+    login: function(req, res){
+        console.log("hi");
+        User.create({name:'Finn', password:"password"}).exec(function(err,result){
+            if (err) {
+                  sails.log.debug('Some error occured ' + err);
+                  return res.json(500, { error: 'Please enter valid user name and password' });
+              }
+              else
+                  {
+                    if(result){
+                     console.log("Success");
+                     res.json(200, {login : "success"});
+                    }
+                  else
+                    {
+                      return res.json(500, { error: 'Please enter valid user name and password' });
+                    }
+                   
+                  } 
+        });
+    ;   
+    }  
+
+   
+
+  };
+
+
+
 
