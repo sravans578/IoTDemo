253160e7a09f9ec8cd5f0f1f320660d103c09174
Sravan <sravan@Sravabns-MacBook-Pro.local>
2019-11-04 09:14:49 -0400

    OTP added fix

--- IoT/api/controllers/UserController.js
+++ IoT/api/controllers/UserController.js
@@ -49,14 +49,6 @@ module.exports = {
         }
         else
         { // On registration success, send an random 4 digit OTP to IoT server. 
-          var otp = Math.random()*4;
-          var request = require('request');
-          request.post({url:devicegw, OTP: otp}, function responseFromIoT(err, httpResponse, body) {
-          if (err) {
-             return console.error('upload failed:', err);
-          }
-             console.log("Response from IoT", body);
-          });
 
           return res.ok({
             success: 'Registration Successful, awaiting OTP response.'
@@ -64,7 +56,49 @@ module.exports = {
         }
       })
    
-  }
+  },
+     
+  Devicelogin: function(req, res){
+    //bicrypt
+          User.find({
+            "DeviceID": req.body.deviceID,
+            "username": req.body.username,
+            "password": req.body.password,
+          }).exec(function(err, result) {
+            if (err) {
+              sails.log.debug('Some error occurred ' + err);
+              return res.ok({
+                error: 'Some error occurred'
+              }, 300);
+    
+            } else {
+              if (result != "") {
+                var otp = Math.random()*4;
+                var request = require('request');
+                request.post({url:devicegw, OTP: otp}, function responseFromIoT(err, httpResponse, body) {
+                if (err) {
+                    return console.error('upload failed:', err);
+                }
+                console.log("Response from IoT", body);
+          });
+
+          
+                //sails.log.debug('Success', JSON.stringify(result));
+
+                return res.ok({
+                  success: 'Login Successful'
+                });
+              } else {
+                return res.ok({
+                  error: ' Please check the username and password'
+                }, 400);
+              }
+            }
+          }); 
+        } , 
+
+    
+     
 
   
 
